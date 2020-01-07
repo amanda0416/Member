92a1141b7c76c5834f4d6f5248839735a1e66b55
miia <miia@com.tw>
2020-01-07 14:41:32 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,7 +2,10 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     String name;
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String userName = getSharedPreferences("test",MODE_PRIVATE)
+                .getString("NAME","");
+        EditText name = findViewById(R.id.nickname);
+        name.setText(userName);
+        //
+        String userAge = getSharedPreferences("test",MODE_PRIVATE)
+                .getString("AGE","");
+        EditText age = findViewById(R.id.age);
+        age.setText(userAge);
+        //
+        String userGender = getSharedPreferences("test",MODE_PRIVATE)
+                .getString("GENDER","");
+        EditText gender = findViewById(R.id.gender);
+        gender.setText(userGender);
+        if (TextUtils.isEmpty(name.getText().toString())&&TextUtils.isEmpty(age.getText().toString())&&TextUtils.isEmpty(gender.getText().toString())){
+            Intent intent  = new Intent(MainActivity.this,NicknameActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
